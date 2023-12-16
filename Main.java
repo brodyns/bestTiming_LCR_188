0b240bea5b28cd69ca19602256cd18cfc82b85d7
brodyns <118648344+brodyns@users.noreply.github.com>
2023-12-16 22:03:16 +0800

    LCR 188. 买卖芯片的最佳时机

---
+++ Main.java
@@ -0,0 +1,20 @@
+﻿public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public int bestTiming(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }else if(prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
